887fccc384679f1e736d6353e4bed87e0f6e2663
mcapps@badgermeter.com <mcapps@badgermeter.com>
2023-09-27 13:55:06 -0500

    script fix

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-poky/oe-init-build-env bmug-build
+cd poky
+oe-init-build-env bmug-build
 
+echo =========================================================
+echo Badger Meter Universal Gateway OS Image Build Environemnt
+echo =========================================================
 echo Build full image:
 echo MACHINE=colibri-imx8x-boug bitbake boug-image-base
 echo or run build-image.sh
@@ -9,3 +13,4 @@ echo Build RAUC update:
 echo MACHINE=colibri-imx8x-boug bitbake update-bundle
 echo or run build-update.sh
 echo Note: a image must be built before the update.
+echo =========================================================
