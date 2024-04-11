0ecbd6f26057975ef003e340967c62e920dfe1ac
mcapps@badgermeter.com <mcapps@badgermeter.com>
2024-04-11 15:26:45 -0500

    path fix in fix script

--- fix-build.sh
+++ fix-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # remove failing file in toradex demos
-if [ -f  layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.bb  ]; then
+if [ -f  ../layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.bb  ]; then
     mv ../layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.bb layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.b-b 
 fi
