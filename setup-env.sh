52df1b6f1d17b128e338bca95bc4f28101b08439
mcapps@badgermeter.com <mcapps@badgermeter.com>
2024-04-11 14:37:22 -0500

    setup script fixed

--- setup-env.sh
+++ setup-env.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
-
-# remove failing file in toradex demos
-if [ -f  layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.bb  ] ; then
-    mv layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.bb layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.b-b 
-fi
+#!/bin/bash
 
 cd poky
-source oe-init-build-env ../bmug-build
+. oe-init-build-env ../bmug-build
 
 echo =========================================================
 echo Badger Meter Universal Gateway OS Image Build Environemnt
@@ -20,3 +15,9 @@ echo MACHINE=colibri-imx8x-boug bitbake update-bundle
 echo or run build-update.sh
 echo Note: a image must be built before the update.
 echo =========================================================
+
+cd ..
+# remove failing file in toradex demos
+if [ -f  layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.bb  ]; then
+    mv layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.bb layers/meta-toradex-demos/recipes-images/images/tdx-reference-multimedia-image.b-b 
+fi
