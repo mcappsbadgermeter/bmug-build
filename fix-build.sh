e216b52400643ccca95cbe41c1eb64c439dde0f0
mcapps@badgermeter.com <mcapps@badgermeter.com>
2024-04-12 09:33:50 -0500

    fix file updated with more files to fix

--- fix-build.sh
+++ fix-build.sh
@@ -5,3 +5,16 @@ localpath="../layers/meta-toradex-demos/recipes-images/images/"
 if [ -f  ${localpath}tdx-reference-multimedia-image.bb  ]; then
     mv ${localpath}tdx-reference-multimedia-image.bb ${localpath}tdx-reference-multimedia-image.b-b 
 fi
+
+# delete
+if [ -f  ${localpath}tdx-reference-minimal-image.bb  ]; then
+    mv ${localpath}tdx-reference-minimal-image.bb ${localpath}tdx-reference-minimal-image.b-b 
+fi
+
+# delete recipes-support/tdx-oak-sensors/tdx-oak-sensors_0082.bb
+localpath="../layers/meta-toradex-demos/recipes-support/tdx-oak-sensors/"
+if [ -f  ${localpath}tdx-oak-sensors_0082.bb  ]; then
+    mv ${localpath}tdx-oak-sensors_0082.bb ${localpath}tdx-oak-sensors_0082.b-b 
+fi
+
+# fix RAUC 
